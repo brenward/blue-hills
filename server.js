2201ca8b31275a103b6a9838dac5ab9908f81cac
brenward <bren.ward@gmail.com>
2017-05-01 11:55:54 +0100

    forcing ssl amend

--- server.js
+++ server.js
@@ -1,23 +1,8 @@
-// Get dependencies
+// server.js
 const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-
-// Get our API routes
-const api = require('./server/routes/api');
-
 const app = express();
 
-// Parsers for POST data
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Point static path to dist
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Set our api routes
-app.use('/api', api);
+const api = require('./server/routes/api');
 
 const forceSSL = function() {
   return function (req, res, next) {
@@ -32,23 +17,17 @@ const forceSSL = function() {
 
 app.use(forceSSL());
 
-// Catch all other routes and return the index file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/index.html'));
+const path = require('path');
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+
+app.use('/api', api);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-/**
- * Get port from environment and store in Express.
- */
-const port = process.env.PORT || '3000';
-app.set('port', port);
-
-/**
- * Create HTTP server.
- */
-const server = http.createServer(app);
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port, () => console.log(`API running on localhost:${port}`));
+app.listen(process.env.PORT || 3000);
